758075278d6a7e590af3b7689e40ebd25876a2df
shash <shash@DESKTOP-A8QAVHA>
2021-07-07 21:38:13 +0530

    ff

---
+++ UnivercitySystem/src/test/abvp.java
@@ -0,0 +1,10 @@
+package test;
+
+public class abvp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- UnivercitySystem/src/test/p1.java
+++ UnivercitySystem/src/test/p1.java
@@ -1,11 +1,11 @@
 package test;
 
-public class p1 
+public class p1
 {
 	public void m1()
 	{
 		//hellow
-		
+		//nbasjkhgfgjksdgh
 	}
 	
 }
